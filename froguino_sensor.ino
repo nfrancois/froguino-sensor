3606b57ae0d858cd5a970f8a9485e7aab2d87c4a
Nicolas FRANCOIS <nicolas.franc@gmail.com>
2013-03-24 17:35:44 +0100

    Initial version

---
+++ froguino_sensor.ino
@@ -0,0 +1,35 @@
+String SEPARATOR = ":";
+// Temperature sensor
+int TEMP_SENSOR_PIN = 0; // analog pin
+String TEMP_SENSOR_NAME = "temp";
+int NUMBER_OF_READING = 10;
+float temperature = 0; 
+// Delay between
+int DELAY = 1000;
+
+
+int i;
+float data;
+
+void setup(){
+  Serial.begin(9600);
+}
+
+void loop(){
+  temperature = 0;
+  for(i = 0;i<NUMBER_OF_READING;i++){
+    // Voltage
+    data = analogRead(TEMP_SENSOR_PIN);
+    // Convert voltage to celsius temperature
+    temperature += ( data * 500.0) / 1024.0;
+    // delay(DELAY);
+  }
+  temperature /= NUMBER_OF_READING; 
+  // Send tempature data
+  Serial.print(TEMP_SENSOR_NAME);
+  Serial.print(SEPARATOR);
+  Serial.println(temperature);
+     
+  // Waiting
+  delay(DELAY);
+}
